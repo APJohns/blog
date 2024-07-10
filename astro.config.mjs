da1fb91bae6b768f8701e6516a1f5125c27eaff0
Ash Johns <mail@ashpjohns.com>
2024-07-10 12:16:31 -0400

    update site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://www.ashjohns.dev',
 	integrations: [mdx(), sitemap()],
   output: 'static',
   adapter: vercel({
