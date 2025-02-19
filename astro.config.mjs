1b1e8179dc46199ddaf76cc673643e70b13b6f0f
Ash Johns <mail@ashpjohns.com>
2025-02-19 13:11:02 -0500

    Add read time to blog pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,17 @@ import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel/static';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-
 import svelte from '@astrojs/svelte';
+
+import { remarkReadingTime } from './plugins/remark-reading-time.mjs';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://www.ashjohns.dev',
-    integrations: [mdx(), sitemap(), svelte()],
+  site: 'https://www.ashjohns.dev',
+  integrations: [mdx(), sitemap(), svelte()],
   output: 'static',
   adapter: vercel({
-    webAnalytics: { enabled: true }
+    webAnalytics: { enabled: true },
   }),
+  markdown: { remarkPlugins: [remarkReadingTime] },
 });
